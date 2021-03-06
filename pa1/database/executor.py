d46445551bf0373990b67b9df32a72094dd7415b
aljazglavac <aljaz.glavac97@gmail.com>
2021-03-06 17:45:28 +0100

    changed naming and fixed filter where

--- pa1/database/executor.py
+++ pa1/database/executor.py
@@ -6,25 +6,25 @@ class SQLExecutor(Database):
         super(SQLExecutor, self).__init__()
         self.schema = 'crawldb'
 
-    def _insert_into_table(self, table, data):
-        insert = 'INSERT INTO {0}.{1} ({2}) values ({3})'.format(
+    def _create(self, table, data):
+        _c = 'INSERT INTO {0}.{1} ({2}) values ({3})'.format(
             self.schema,
             table,
             ','.join(data.keys()),
             ','.join(['%({})s'.format(k) for k in data.keys()]))
 
         with self.transaction() as cursor:
-            query = cursor.mogrify(insert, data)
+            query = cursor.mogrify(_c, data)
             cursor.execute(query)
 
-    def _read_from_table(self, table, fields, fetch_all=True):
-        read = 'SELECT {0} FROM {1}.{2}'.format(
+    def _list(self, table, fields, fetch_all=True):
+        _l = 'SELECT {0} FROM {1}.{2}'.format(
             ','.join(fields),
             self.schema,
             table)
 
         with self.transaction() as cursor:
-            query = cursor.mogrify(read)
+            query = cursor.mogrify(_l)
             cursor.execute(query)
 
             if fetch_all:
@@ -32,15 +32,15 @@ class SQLExecutor(Database):
             else:
                 return cursor.fetchone()
 
-    def _filter_table(self, table, fields, data, fetch_all=True):
-        filter = 'SELECT {0} FROM {1}.{2} WHERE {3}'.format(
+    def _filter(self, table, fields, data, fetch_all=True):
+        _f = 'SELECT {0} FROM {1}.{2} WHERE {3}'.format(
             ','.join(fields),
             self.schema,
             table,
-            ",".join(["{0}={1}".format(k, v) for k, v in data.items()]))
+            ",".join(["{0}='{1}'".format(k, v) for k, v in data.items()]))
 
         with self.transaction() as cursor:
-            query = cursor.mogrify(filter)
+            query = cursor.mogrify(_f)
             cursor.execute(query)
 
             if fetch_all:
