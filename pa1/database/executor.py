d3175565f35c4d921b8e9560cdf2f68fa287ee33
aljazglavac <aljaz.glavac97@gmail.com>
2021-03-08 06:03:30 +0100

    if debug then print formatted query and added RETURNING to create

--- pa1/database/executor.py
+++ pa1/database/executor.py
@@ -7,21 +7,30 @@ class SQLExecutor(Database):
         self.schema = 'crawldb'
 
     def _create(self, table, data):
-        _c = 'INSERT INTO {0}.{1} ({2}) values ({3})'.format(
+        _c = 'INSERT INTO {0}.{1} ({2}) values ({3}) RETURNING *'.format(
             self.schema,
             table,
             ','.join(data.keys()),
             ','.join(['%({})s'.format(k) for k in data.keys()]))
 
+        if self.DEBUG:
+            print("create - ", _c)
+
         with self.transaction() as cursor:
             query = cursor.mogrify(_c, data)
             cursor.execute(query)
+            _created = cursor.fetchone()
+            return _created
 
-    def _list(self, table, fields, fetch_all=True):
-        _l = 'SELECT {0} FROM {1}.{2}'.format(
+    def _list(self, table, fields, fetch_all=True, order_by=['-id']):
+        _l = 'SELECT {0} FROM {1}.{2} ORDER BY {3}'.format(
             ','.join(fields),
             self.schema,
-            table)
+            table,
+            ','.join(order_by))
+
+        if self.DEBUG:
+            print("list - ", _l)
 
         with self.transaction() as cursor:
             query = cursor.mogrify(_l)
@@ -32,12 +41,16 @@ class SQLExecutor(Database):
             else:
                 return cursor.fetchone()
 
-    def _filter(self, table, fields, data, fetch_all=True):
-        _f = 'SELECT {0} FROM {1}.{2} WHERE {3}'.format(
+    def _filter(self, table, fields, data, fetch_all=True, order_by=['-id']):
+        _f = 'SELECT {0} FROM {1}.{2} WHERE {3} ORDER BY {4}'.format(
             ','.join(fields),
             self.schema,
             table,
-            ",".join(["{0}='{1}'".format(k, v) for k, v in data.items()]))
+            ','.join(["{0}='{1}'".format(k, v) for k, v in data.items()]),
+            ','.join(order_by))
+
+        if self.DEBUG:
+            print("filter - ", _f)
 
         with self.transaction() as cursor:
             query = cursor.mogrify(_f)
