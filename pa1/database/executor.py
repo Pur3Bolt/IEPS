8a57216534f917ca4545c6e54d949184b72d8a60
aljazglavac <aljaz.glavac97@gmail.com>
2021-03-10 05:30:59 +0100

    join statement parser

--- pa1/database/executor.py
+++ pa1/database/executor.py
@@ -1,4 +1,5 @@
 from .connector import Database
+from psycopg2 import extras
 
 
 class SQLExecutor(Database):
@@ -76,3 +77,31 @@ class SQLExecutor(Database):
             cursor.execute(query)
             _updated = cursor.fetchone()
             return _updated
+
+    def _join(self, table_1, table_2, kind, on, filters, fields='*', fetch_all=True, order_by=None):
+        _j = 'SELECT {0} FROM {1}.{2} t1 {4} JOIN {1}.{3} t2 ON {5} {6} {7}'.format(
+            ','.join(fields),
+            self.schema,
+            table_1,
+            table_2,
+            kind.upper(),
+            ','.join(["t1.{0}=t2.{1}".format(k, v) for k, v in on.items()]),
+            'WHERE '+','.join(["{0}='{1}'".format(k, v) for k, v in filters.items()]) if filters else "",
+            'ORDER BY '+','.join(list(map(lambda x: "-{0}.{1}".format(x.replace('-', ''), table_1)
+                                          if '-' in x
+                                          else "{0}.{1}".format(x, table_1), order_by))) if order_by else ""
+        )
+
+        if self.DEBUG:
+            print("join - ", _j)
+
+        # use DictCursor so we can deal with duplicated column names
+        self.cursor_factory = extras.DictCursor
+        with self.transaction() as cursor:
+            query = cursor.mogrify(_j)
+            cursor.execute(query)
+
+            if fetch_all:
+                return cursor.fetchall()
+            else:
+                return cursor.fetchone()
