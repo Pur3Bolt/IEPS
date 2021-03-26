b08607e53edee1089b99193aa710147a32f167a0
Lequicky <andrejkokelj@gmail.com>
2021-03-26 09:23:09 +0100

    Update _crawler.py

--- pa1/_crawler.py
+++ pa1/_crawler.py
@@ -375,11 +375,13 @@ class Crawler:
                     status_code, is_html, content_type = 0, False, 'text/html'
                     if self.driver.current_url == 'data:,':
                         for request in self.driver.requests:
-                            if request.response and request.response.headers['Content-Type'] is not None:
-                                is_html = 'text/html' in request.response.headers['Content-Type']
-                                content_type = request.response.headers['Content-Type']
-                            else:
-                                content_type = 'unknown'
+                            if request.response:
+                                status_code = request.response.status_code
+                                if request.response.headers['Content-Type'] is not None:
+                                    is_html = 'text/html' in request.response.headers['Content-Type']
+                                    content_type = request.response.headers['Content-Type']
+                                else:
+                                    content_type = 'unknown'
                     else:
                         for request in self.driver.requests:
                             if request.response and self.url_to_canon(request.url) == self.url_to_canon(self.driver.current_url):
